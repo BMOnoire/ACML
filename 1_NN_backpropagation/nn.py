35bba6733dab2a5ff70ced01ac5e06f2a5f44abf
Glauco <glauco.lorenzut@gmail.com>
2020-10-31 13:31:36 +0100

    Fixed NN

--- 1_NN_backpropagation/nn.py
+++ 1_NN_backpropagation/nn.py
@@ -1,9 +1,24 @@
 import numpy as np
+from datetime import datetime
+import time
+
+
 
 INPUT_LAYER = 8
 HIDDEN_LAYER = 3
 OUTPUT_LAYER = 8
+EPOCHS = 10000
 
+DATASET = [
+    np.array([1, 0, 0, 0, 0, 0, 0, 0]),
+    np.array([0, 1, 0, 0, 0, 0, 0, 0]),
+    np.array([0, 0, 1, 0, 0, 0, 0, 0]),
+    np.array([0, 0, 0, 1, 0, 0, 0, 0]),
+    np.array([0, 0, 0, 0, 1, 0, 0, 0]),
+    np.array([0, 0, 0, 0, 0, 1, 0, 0]),
+    np.array([0, 0, 0, 0, 0, 0, 1, 0]),
+    np.array([0, 0, 0, 0, 0, 0, 0, 1])
+]
 
 class NN:
 
@@ -11,42 +26,76 @@ class NN:
         self.input_size  = n_inputs
         self.hidden_size = n_hidden
         self.output_size = n_outputs
-        self.weights_0L  = np.random.randn(n_inputs, n_hidden)
-        self.weights_1L  = np.random.randn(n_hidden, n_outputs)
-        self.bias_0L     = np.ones((1, n_hidden))
-        self.bias_1L     = np.ones((1, n_outputs))
+        self.weights_0L  = np.random.randn(n_hidden, n_inputs)
+        self.weights_1L  = np.random.randn(n_outputs, n_hidden)
+        self.bias_0L     = np.ones((n_hidden, 1))
+        self.bias_1L     = np.ones((n_outputs, 1))
 
 
     def __sigmoid(self, x):
         return 1.0 / (1.0 + np.exp(-x))
 
 
-    def __derivate_sigmoid(self, x):
+    def __relu(self, x):
+        return max(x, 0)
+
+
+    def __derivative_sigmoid(self, x):
         return (np.exp(-x)) / ((np.exp(-x) + 1.0) ** 2)
 
 
-    def forward_propagation(self, input):
-        if len(input) != self.input_size:
-            print("ERROR: input size is not suitable for NN")
-            return None
-        hidden_layer = self.__sigmoid(input)
-        hidden_layer = np.dot(input, self.weights_0L) + self.bias_0L
-        hidden_layer = self.__sigmoid(hidden_layer[0])
-        output = np.dot(hidden_layer, self.weights_1L) + self.bias_1L
-        output = self.__sigmoid(output[0])
-        for a in output:
-            print(a)
-        return output
+    def __derivate_relu(self, x):
+        return 1 if x > 0 else 0
+
+
+    def __feed_forward(self, input):
+        input = np.array([input]).T
+        # sigm( (W0 * I) + b0 ) = H
+        hidden_layer = self.__sigmoid(np.dot(self.weights_0L, input) + self.bias_0L)
+        # sigm( (W1 * H) + b1 ) = O
+        output = self.__sigmoid(np.dot(self.weights_1L, hidden_layer) + self.bias_1L)
+        return output.T[0]
+
+    def __back_propagation(self, obtained_output, expected_output):
+        diff_vec = obtained_output - expected_output
+        cost = 0
+        for val_d in diff_vec:
+            cost += diff_vec**2
+        cost = 0.5*cost
+        print(cost)
+        derivative_cost = diff_vec * self.__derivative_sigmoid(obtained_output)
+
+
+
+
+        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+        #d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
+        #d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
+
+        # update the weights with the derivative (slope) of the loss function
+        #self.weights1 += d_weights1
+        #self.weights2 += d_weights2
+
+        return 1
+
+    def train(self, dataset, epochs):
+        for epoch in range(epochs):
+            for input in dataset:
+                output = self.__feed_forward(input)
+                asd = self.__back_propagation(output, input)
 
 
 def main():
-    dataset = np.eye(INPUT_LAYER)
-
     neural_network = NN(INPUT_LAYER, HIDDEN_LAYER, OUTPUT_LAYER)
-    for element in dataset:
-        qwe = neural_network.forward_propagation(element)
-        print(qwe)
 
+    mean_time = 0
+    #for i in range(10):
+    start_time = datetime.now()
+    neural_network.train(DATASET, EPOCHS)
+    end_time = datetime.now() - start_time
+    mean_time += end_time.total_seconds()
+
+    #print("Time spent: " + str(mean_time/10))
 
 
 if __name__ == '__main__':
