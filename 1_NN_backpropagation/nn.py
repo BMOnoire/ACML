a483b81f3f7c594207688f5e4497aaa74b56f26a
Glauco <glauco.lorenzut@gmail.com>
2020-11-02 12:24:49 +0100

    Changed names

--- 1_NN_backpropagation/nn.py
+++ 1_NN_backpropagation/nn.py
@@ -26,49 +26,74 @@ class NN:
         self.input_size  = n_inputs
         self.hidden_size = n_hidden
         self.output_size = n_outputs
-        self.weights_0L  = np.random.randn(n_hidden, n_inputs)
-        self.weights_1L  = np.random.randn(n_outputs, n_hidden)
-        self.bias_0L     = np.ones((n_hidden, 1))
-        self.bias_1L     = np.ones((n_outputs, 1))
+        self.W1  = np.random.randn(n_hidden, n_inputs)
+        self.W2  = np.random.randn(n_outputs, n_hidden)
+        self.B1    = np.ones((n_hidden, 1))
+        self.B2     = np.ones((n_outputs, 1))
+        self.A1 = None
+        self.A2 = None
 
 
-    def __sigmoid(self, x):
-        return 1.0 / (1.0 + np.exp(-x))
+    def __sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
 
 
-    def __relu(self, x):
-        return max(x, 0)
+    def __derivative_sigmoid(self, z):
+        return self.__sigmoid(z) * (1-self.__sigmoid(z))
 
 
-    def __derivative_sigmoid(self, x):
-        return (np.exp(-x)) / ((np.exp(-x) + 1.0) ** 2)
+    def __relu(self, z):
+        return max(z, 0)
 
 
-    def __derivate_relu(self, x):
-        return 1 if x > 0 else 0
+    def __derivate_relu(self, z):
+        return 1 if z > 0 else 0
 
 
     def __feed_forward(self, input):
         input = np.array([input]).T
-        # sigm( (W0 * I) + b0 ) = H
-        hidden_layer = self.__sigmoid(np.dot(self.weights_0L, input) + self.bias_0L)
-        # sigm( (W1 * H) + b1 ) = O
-        output = self.__sigmoid(np.dot(self.weights_1L, hidden_layer) + self.bias_1L)
-        return output.T[0]
+        # sigm( (W1 * I) + B1 ) = A1
+        self.A1 = self.__sigmoid(np.dot(self.W1, input) + self.B1)
+        # sigm( (W2 * A1) + B2 ) = A2
+        self.A2 = self.__sigmoid(np.dot(self.W2, self.A1) + self.B2)
+        return self.A2.T[0]
 
-    def __back_propagation(self, obtained_output, expected_output):
-        diff_vec = obtained_output - expected_output
+
+    def __back_propagation(self, Y, Y_pred):
+        diff_vec_Y = Y - Y_pred
         cost = 0
         for val_d in diff_vec:
             cost += diff_vec**2
         cost = 0.5*cost
         print(cost)
-        derivative_cost = diff_vec * self.__derivative_sigmoid(obtained_output)
+        derivative_cost = diff_vec * self.__derivative_sigmoid(Y)
+        diff_matrix_1L = np.dot(self.weights_1L.T, derivative_cost)
+        diff_matrix_0L = np.dot(self.input.T,  (np.dot(derivative_cost, self.weights2.T) * sigmoid_derivative(self.layer1)))
+
+        def back_propagate(W1, b1, W2, b2, cache):
+            # Retrieve also A1 and A2 from dictionary "cache"
+            A1 = cache['A1']
+            A2 = cache['A2']
+
+            # Backward propagation: calculate dW1, db1, dW2, db2.
+            dZ2 = A2 - Y
+            dW2 = (1 / m) * np.dot(dZ2, A1.T)
+            db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+
+            dZ1 = np.multiply(np.dot(W2.T, dZ2), 1 - np.power(A1, 2))
+            dW1 = (1 / m) * np.dot(dZ1, X.T)
+            db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+
+            # Updating the parameters according to algorithm
+            W1 = W1 - learning_rate * dW1
+            b1 = b1 - learning_rate * db1
+            W2 = W2 - learning_rate * dW2
+            b2 = b2 - learning_rate * db2
+
+            return W1, W2, b1, b2
 
 
-
-
-        # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+            # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
         #d_weights2 = np.dot(self.layer1.T, (2*(self.y - self.output) * sigmoid_derivative(self.output)))
         #d_weights1 = np.dot(self.input.T,  (np.dot(2*(self.y - self.output) * sigmoid_derivative(self.output), self.weights2.T) * sigmoid_derivative(self.layer1)))
 
