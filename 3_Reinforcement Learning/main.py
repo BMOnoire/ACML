18b0d0d313f2461afba87749110588fd950cd934
Glauco <glauco.lorenzut@gmail.com>
2020-11-20 20:47:40 +0100

    Added Q-learning

--- 3_Reinforcement Learning/main.py
+++ 3_Reinforcement Learning/main.py
@@ -1,231 +1,81 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # lighter system log
-
-import tensorflow as tf
+import gym
 import numpy as np
-import matplotlib.pyplot as plt
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # lighter system log
 import config as cfg
-import nn
-import time
 import janitor as jn
-import cv2
 
-TEST_STANDARD = True
-TEST_SMART = True
-TEST_COLORIZER = True
+# ACTIONS
+GO_LEFT, STAY, GO_RIGHT = 0, 1, 2
 
-# just to check if GPU is working
-if tf.test.gpu_device_name():
-    print('Default GPU Device:{}'.format(tf.test.gpu_device_name()))
-else:
-   print("Please install GPU version of TF")
-
-
-def extract_data():
-    dataset_rgb = tf.keras.datasets.cifar10.load_data()
-    dataset_rgb = np.vstack((dataset_rgb[0][0], dataset_rgb[1][0]))  # merged train and test set
-
-    dataset_yuv = np.array([ cv2.cvtColor(img, cv2.COLOR_RGB2YUV) for img in dataset_rgb])
-
-    # normalize image values from 0-255 to 0-1
-    dataset_rgb_norm = dataset_rgb / 255.
-    dataset_yuv_norm = dataset_yuv / 255.
-
-    # recreate another set of (train, validation, test)
-    size = len(dataset_rgb)
-    return [
-        (dataset_rgb[:int(size * 0.8)], dataset_rgb[int(size * 0.8):int(size * 0.9)], dataset_rgb[int(size * 0.9):]),
-        (dataset_rgb_norm[:int(size * 0.8)], dataset_rgb_norm[int(size * 0.8):int(size * 0.9)], dataset_rgb_norm[int(size * 0.9):]),
-        (dataset_yuv[:int(size * 0.8)], dataset_yuv[int(size * 0.8):int(size * 0.9)], dataset_yuv[int(size * 0.9):]),
-        (dataset_yuv_norm[:int(size * 0.8)], dataset_yuv_norm[int(size * 0.8):int(size * 0.9)], dataset_yuv_norm[int(size * 0.9):])
-    ]
-
-
-def plot_hist_accuracy(history, img_name):
-    f, ax = plt.subplots(1, 1)
-    ax.plot(history.history['accuracy'], c='C0')
-    ax.plot(history.history['val_accuracy'], c='C1')
-    ax.set_title('Model accuracy')
-    ax.set_ylabel('Accuracy')
-    ax.set_xlabel('Epoch')
-    ax.legend(['Train', 'Test'], loc='upper left')
-    plt.savefig(str(cfg.general["imgs_path"] / img_name))
-    #plt.show()
-
-
-def plot_hist_loss(history, img_name):
-    f, ax = plt.subplots(1, 1)
-    ax.plot(history.history['loss'], c='C0')
-    ax.plot(history.history['val_loss'], c='C1')
-    ax.set_title('Model loss')
-    ax.set_ylabel('Loss')
-    ax.set_xlabel('Epoch')
-    ax.legend(['Train', 'Test'], loc='upper left')
-    plt.savefig(str(cfg.general["imgs_path"] / img_name))
-    #plt.show()
-
-
-def plot_images_resulting(real_img_set, predicted_img_set, filter, saving_name, index_list=None):
-    cv2.namedWindow('image', cv2.WINDOW_NORMAL)
-    cv2.resizeWindow('image', 1600, 1600)
-
-    if not index_list:
-        index_list = range(5)
-
-    # avoid stupid crashes
-    real_img_set = np.float32(real_img_set)
-    predicted_img_set = np.float32(predicted_img_set)
-
-    # add a small border for initialization
-    result_w = real_img_set[0].shape[1] + predicted_img_set[0].shape[1]
-    result = np.zeros((1, result_w, real_img_set[0].shape[2]))
-
-
-    for i in index_list:
-        # reconstruct image
-        real_img = np.float32(real_img_set[i])
-        pred_img = np.float32(predicted_img_set[i])
-
-        real_image = cv2.cvtColor(real_img, filter)
-        predicted_image = cv2.cvtColor(pred_img, filter)
-
-        result = np.vstack([result, np.hstack([real_image, predicted_image])])
-    ###
-
-    cv2.imshow('image', result)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
-    #result = np.around(result*255)
-    #result = np.array(result, np.int)
-    ##image.convertTo(result, cv2.CV_8UC3, 255.0)
-    #cv2.imwrite("asd.png", result)
-
-    #n = 5
-    #plt.figure(figsize=(32, 4))
-    #for i in range(1, n + 1):
-    #    # Display original
-    #    ax = plt.subplot(2, n, i)
-    #    plt.imshow(dataset[i])
-    #    plt.gray()
-    #    ax.get_xaxis().set_visible(False)
-    #    ax.get_yaxis().set_visible(False)
-
-    #    # Display reconstruction
-    #    ax = plt.subplot(2, n, i + n)
-    #    plt.imshow(predicted_dataset[i])
-    #    plt.gray()
-    #    ax.get_xaxis().set_visible(False)
-    #    ax.get_yaxis().set_visible(False)
-    #plt.savefig(str(cfg.general["imgs_path"] / img_name))
-    #plt.show()
-
-
+Q_TABLE_DIMENSION = 20
+LEARNING_RATE = 0.1
+DISCOUNT = 0.95
+EPSILON = 0.5
+EPISODES = 30000
+SHOW_EVERY = EPISODES // 50
+START_EPSILON_DECAYING = 1
+END_EPSILON_DECAYING = EPISODES // 2
+EPSILON_DECAY_VALUE = EPSILON / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
 def main():
-    jn.create_dir(cfg.general["imgs_path"])
-    jn.create_dir(cfg.general["models_path"])
+    np.random.seed(1)
+    env = gym.make("MountainCar-v0")
+    epsilon = EPSILON
 
-    start = time.time()
-    pickle_dataset_path = cfg.general["pickle_path"] / "dataset.pickle"
-    dataset_list = jn.pickle_load(pickle_dataset_path)
-    if not dataset_list:
-        jn.create_dir(cfg.general["pickle_path"])
-        dataset_list = extract_data()
-        jn.pickle_save(dataset_list, pickle_dataset_path)
+    observation_size = len(env.observation_space.high)
+    discrete_observation_size = [Q_TABLE_DIMENSION] * observation_size
+    discrete_observation_steps = (env.observation_space.high - env.observation_space.low) / discrete_observation_size
 
-    print("\nExtraction and preprocessing time: {:0.3f} sec\n".format(time.time() - start))
+    q_table_size = discrete_observation_size + [env.action_space.n]
+    q_table = np.random.uniform(low=-2, high=0, size=(q_table_size)) #(20, 20, 3)
 
-    dataset_rgb, dataset_rgb_norm, dataset_yuv, dataset_yuv_norm = dataset_list
+    def get_discrete_state(state): # the indexes about what state we are
+        discrete_state = (state - env.observation_space.low) / discrete_observation_steps
+        return tuple(discrete_state.astype(np.int))
 
-    train_dataset, evaluation_dataset, test_dataset = dataset_rgb_norm[0], dataset_rgb_norm[1], dataset_rgb_norm[2]
-    ####################################################################################################################
-    if TEST_STANDARD:
+    for episode in range(EPISODES):
+        discrete_state = get_discrete_state(env.reset())
 
+        if episode % SHOW_EVERY == 0:
+            print(episode)
+            render = True
+        else:
+            render = False
 
-        cnn_standard = nn.NN((train_dataset, train_dataset), (evaluation_dataset, evaluation_dataset), (test_dataset, test_dataset))
-        cnn_standard.init_standard_convolutional_autoencoder()
-
-        start = time.time()
-        hist_standard, layers_standard = cnn_standard.train_model(str(cfg.general["models_path"] / "standard_model.h5"))
-        print("\nCNN standard training time: {:0.3f} sec\n".format(time.time() - start))
-
-
-        plot_hist_accuracy(hist_standard, "accuracy_standard.png")
-        plot_hist_loss(hist_standard, "loss_standard.png")
-        predicted_dataset = cnn_standard.predict_output(test_dataset)
-        plot_images_resulting(test_dataset, predicted_dataset, cv2.COLOR_BGR2RGB, "confront_standard.png", cfg.general["images_list"])
-
-        hist_standard_path = cfg.general["pickle_path"] / "hist_standard.pickle"
-        jn.pickle_save(hist_standard.history, hist_standard_path)
-
-        test_loss_standard, test_accuracy_standard = cnn_standard.evaluate_model()
-        print("Test Accuracy standard -> ", test_loss_standard)
-        print("Test Loss standard -> ", test_accuracy_standard)
-    ####################################################################################################################
+        done = False
+        while not done:
+            if np.random.random() > epsilon:
+                action = np.argmax(q_table[discrete_state])
+            else:
+                action = np.random.randint(0, env.action_space.n)
 
 
-    ####################################################################################################################
-    if TEST_SMART:
-        cnn_smart = nn.NN((train_dataset, train_dataset), (evaluation_dataset, evaluation_dataset), (test_dataset, test_dataset))
-        cnn_smart.init_smart_convolutional_autoencoder()
-
-        start = time.time()
-        hist_smart, layers_smart = cnn_smart.train_model(str(cfg.general["models_path"] / "smart_model.h5"))
-        print("\nCNN smart training time: {:0.3f} sec\n".format(time.time() - start))
-
-        plot_hist_accuracy(hist_smart, "accuracy_smart.png")
-        plot_hist_loss(hist_smart, "loss_smart.png")
-        predicted_dataset = cnn_smart.predict_output(test_dataset)
-        plot_images_resulting(test_dataset, predicted_dataset, cv2.COLOR_BGR2RGB, "confront_smart.png", cfg.general["images_list"])
-
-        hist_smart_path = cfg.general["pickle_path"] / "hist_smart.pickle"
-        jn.pickle_save(hist_smart.history, hist_smart_path)
-
-        test_loss_smart, test_accuracy_smart = cnn_smart.evaluate_model()
-        print("Test Accuracy smart -> ", test_loss_smart)
-        print("Test Loss smart -> ", test_accuracy_smart)
-    ####################################################################################################################
 
 
-    ####################################################################################################################
-    if TEST_COLORIZER:
-        train_dataset, evaluation_dataset, test_dataset = dataset_yuv_norm[0], dataset_yuv_norm[1], dataset_yuv_norm[2]
-        train_input, evaluation_input, test_input    = np.expand_dims(train_dataset[:, :, :, 0], axis=3), np.expand_dims(evaluation_dataset[:, :, :, 0], axis=3), np.expand_dims(test_dataset[:, :, :, 0], axis=3)
-        train_output, evaluation_output, test_output = train_dataset[:, :, :, 1:], evaluation_dataset[:, :, :, 1:], test_dataset[:, :, :, 1:]
 
-        cnn_colorizer = nn.NN((train_input, train_output), (evaluation_input, evaluation_output), (test_input, test_output))
-        cnn_colorizer.init_colorizer_convolutional_autoencoder()
+            new_state, reward, done, env_msg = env.step(action)
+            new_discrete_state = get_discrete_state(new_state)
 
-        start = time.time()
-        hist_colorizer, layers_colorizer = cnn_colorizer.train_model(str(cfg.general["models_path"] / "colorizer_model.h5"))
-        print("\nCNN colorizer training time: {:0.3f} sec\n".format(time.time() - start))
+            if render:
+                env.render()
 
-        plot_hist_accuracy(hist_colorizer, "accuracy_colorizer.png")
-        plot_hist_loss(hist_colorizer, "loss_colorizer.png")
+            if not done:
+                max_future_q = np.max(q_table[new_discrete_state])
+                current_q = q_table[discrete_state + (action,  )]
 
-        dataset_real_y = np.expand_dims(test_dataset[:, :, :, 0], axis=3)
-        dataset_predicted_uv = cnn_colorizer.predict_output(dataset_real_y)
+                new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+                q_table[discrete_state + (action, )] = new_q
+            elif new_state[0] >= env.goal_position:
+                print("Reached the goal on episode", episode)
+                q_table[discrete_state + (action, )] = 0
 
-        def predict_YUV_images(Y_list, UV_list):
-            U_list = np.expand_dims(UV_list[:, :, :, 0], axis=3)
-            V_list = np.expand_dims(UV_list[:, :, :, 1], axis=3)
-            return np.concatenate((Y_list, U_list, V_list), axis=3) #np.dstack((u, u, v))
-            #pred_img = cv2.merge((y, u, v))
+            discrete_state = new_discrete_state
 
+        if START_EPSILON_DECAYING <= episode <= END_EPSILON_DECAYING:
+            epsilon -= EPSILON_DECAY_VALUE
 
-        predicted_dataset = predict_YUV_images(dataset_real_y, dataset_predicted_uv)
-
-        plot_images_resulting(test_dataset, predicted_dataset, cv2.COLOR_YUV2BGR, "confront_colorized.png", cfg.general["images_list"])
-
-        hist_colorizer_path = cfg.general["pickle_path"] / "hist_colorizer.pickle"
-        jn.pickle_save(hist_colorizer.history, hist_colorizer_path)
-
-        test_loss_colorizer, test_accuracy_colorizer = cnn_colorizer.evaluate_model()
-        print("Test Accuracy colorizer -> ", test_loss_colorizer)
-        print("Test Loss colorizer -> ", test_accuracy_colorizer)
-    ####################################################################################################################
+    env.close()
 
 
  
