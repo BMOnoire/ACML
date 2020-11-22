81b0c4bb6995ccefd2abdc72de529f59772a02d0
Glauco <glauco.lorenzut@gmail.com>
2020-11-23 00:57:04 +0100

    Added stats plotting

--- 3_Reinforcement Learning/main.py
+++ 3_Reinforcement Learning/main.py
@@ -1,6 +1,7 @@
 import os
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # lighter system log
 import config as cfg
 import janitor as jn
@@ -11,12 +12,20 @@ GO_LEFT, STAY, GO_RIGHT = 0, 1, 2
 Q_TABLE_DIMENSION = 20
 LEARNING_RATE = 0.1
 DISCOUNT = 0.95
-EPSILON = 0.5
-EPISODES = 30000
-SHOW_EVERY = EPISODES // 50
+EPSILON = 0.2
+EPISODES = 3000
+SHOW_EVERY = EPISODES // 20
 START_EPSILON_DECAYING = 1
 END_EPSILON_DECAYING = EPISODES // 2
 EPSILON_DECAY_VALUE = EPSILON / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
+
+ep_rewards = []
+aggr_ep_rewards = {
+    "ep": [],
+    "avg": [],
+    "min": [],
+    "max": []
+}
 
 def main():
     np.random.seed(1)
@@ -34,7 +43,8 @@ def main():
         discrete_state = (state - env.observation_space.low) / discrete_observation_steps
         return tuple(discrete_state.astype(np.int))
 
-    for episode in range(EPISODES):
+    for episode in range(EPISODES+1):
+        episode_reward = 0
         discrete_state = get_discrete_state(env.reset())
 
         if episode % SHOW_EVERY == 0:
@@ -50,11 +60,10 @@ def main():
             else:
                 action = np.random.randint(0, env.action_space.n)
 
-
-
-
-
             new_state, reward, done, env_msg = env.step(action)
+
+            episode_reward += reward
+
             new_discrete_state = get_discrete_state(new_state)
 
             if render:
@@ -75,7 +84,24 @@ def main():
         if START_EPSILON_DECAYING <= episode <= END_EPSILON_DECAYING:
             epsilon -= EPSILON_DECAY_VALUE
 
+        ep_rewards.append(episode_reward)
+
+        if not episode % SHOW_EVERY:
+            average_reward = sum(ep_rewards[-SHOW_EVERY:])/len(ep_rewards[-SHOW_EVERY:])
+            aggr_ep_rewards["ep"].append(episode)
+            aggr_ep_rewards["avg"].append(average_reward)
+            aggr_ep_rewards["min"].append(min(ep_rewards[-SHOW_EVERY:]))
+            aggr_ep_rewards["max"].append(max(ep_rewards[-SHOW_EVERY:]))
+
+            print("Episode:", episode, "average:", average_reward, "min:", min(ep_rewards[-SHOW_EVERY:]), "max:", max(ep_rewards[-SHOW_EVERY:]))
+
     env.close()
+
+    plt.plot(aggr_ep_rewards["ep"], aggr_ep_rewards["avg"], label="avg")
+    plt.plot(aggr_ep_rewards["ep"], aggr_ep_rewards["min"], label="min")
+    plt.plot(aggr_ep_rewards["ep"], aggr_ep_rewards["max"], label="max")
+    plt.legend(loc=4)
+    plt.show()
 
 
  
