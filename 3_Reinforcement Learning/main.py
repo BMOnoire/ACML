5bf37badd9728750e043df83088db1bf8fd26ccf
Glauco <glauco.lorenzut@gmail.com>
2020-11-23 21:30:01 +0100

    Added the test factory

--- 3_Reinforcement Learning/main.py
+++ 3_Reinforcement Learning/main.py
@@ -2,56 +2,105 @@ import os
 import gym
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 #os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # lighter system log
 import config as cfg
 import janitor as jn
 
+#TODO
+# heatmap 20.60 -> 21
+
+
 # ACTIONS
 GO_LEFT, STAY, GO_RIGHT = 0, 1, 2
 
-Q_TABLE_DIMENSION = 20
-LEARNING_RATE = 0.1
-DISCOUNT = 0.95
-EPSILON = 0.2
-EPISODES = 3000
-SHOW_EVERY = EPISODES // 20
-START_EPSILON_DECAYING = 1
-END_EPSILON_DECAYING = EPISODES // 2
-EPSILON_DECAY_VALUE = EPSILON / (END_EPSILON_DECAYING - START_EPSILON_DECAYING)
 
-ep_rewards = []
-aggr_ep_rewards = {
-    "ep": [],
-    "avg": [],
-    "min": [],
-    "max": []
-}
+EPOCHS = 5000
 
-def main():
+TEST_LIST = [
+    {
+        "id": "first",
+        "epochs": EPOCHS,
+        "show_n_time": 0,
+        "q_table_dimension": 10,
+        "learning_rate": 0.1,
+        "discount": 0.95,
+        "epsilon": 0.5,
+        "epsilon_decaying_range": (1, EPOCHS//2)
+    },
+    {
+        "id": "second",
+        "epochs": EPOCHS,
+        "show_n_time": 0,
+        "q_table_dimension": 20,
+        "learning_rate": 0.1,
+        "discount": 0.95,
+        "epsilon": 0.5,
+        "epsilon_decaying_range": (1, EPOCHS//2)
+    },
+    {
+        "id": "third",
+        "epochs": EPOCHS,
+        "show_n_time": 0,
+        "q_table_dimension": 40,
+        "learning_rate": 0.1,
+        "discount": 0.95,
+        "epsilon": 0.5,
+        "epsilon_decaying_range": (1, EPOCHS//2)
+    }
+]
+
+
+
+def plot_graph_result(epoch_list, avg_list, max_list, min_list):
+    plt.plot(epoch_list, avg_list, label="avg", color="green")
+    plt.plot(epoch_list, max_list, label="max", color="red")
+    plt.plot(epoch_list, min_list, label="min", color="blue")
+    plt.legend(loc='upper left')
+    plt.show()
+
+def plot_heat_map():
+    pass
+
+
+def launch_new_q_learning_test(epoch_number, n_show, q_table_dimension, learning_rate, discount, epsilon, epsilon_decaying_range):
+    epoch_rewards = []
+    aggr_ep_rewards = {
+        "epoch": [],
+        "avg_value": [],
+        "min_value": [],
+        "max_value": []
+    }
+
     np.random.seed(1)
     env = gym.make("MountainCar-v0")
-    epsilon = EPSILON
 
-    observation_size = len(env.observation_space.high)
-    discrete_observation_size = [Q_TABLE_DIMENSION] * observation_size
+    observation_size = len(env.observation_space.high) # S, V
+    discrete_observation_size = [q_table_dimension] * observation_size
     discrete_observation_steps = (env.observation_space.high - env.observation_space.low) / discrete_observation_size
 
     q_table_size = discrete_observation_size + [env.action_space.n]
-    q_table = np.random.uniform(low=-2, high=0, size=(q_table_size)) #(20, 20, 3)
+    q_table = np.random.uniform(low=-2, high=0, size=(q_table_size)) #(N, N, 3)
+
+    first_goal = True
+    show_range = epoch_number // n_show if n_show else epoch_number + 1
+    save_range = epoch_number // 100 if epoch_number > 100 else 1
+    epsilon_decay_value = epsilon / (epsilon_decaying_range[1] - epsilon_decaying_range[0])
 
     def get_discrete_state(state): # the indexes about what state we are
         discrete_state = (state - env.observation_space.low) / discrete_observation_steps
         return tuple(discrete_state.astype(np.int))
 
-    for episode in range(EPISODES+1):
-        episode_reward = 0
+    start = time.time()
+    for epoch in range(1, epoch_number+1):
+        #print(epoch)
+
+        epoch_reward = 0
+
         discrete_state = get_discrete_state(env.reset())
 
-        if episode % SHOW_EVERY == 0:
-            print(episode)
-            render = True
-        else:
-            render = False
+
+        render = True if not epoch % show_range else False
 
         done = False
         while not done:
@@ -62,7 +111,7 @@ def main():
 
             new_state, reward, done, env_msg = env.step(action)
 
-            episode_reward += reward
+            epoch_reward += reward
 
             new_discrete_state = get_discrete_state(new_state)
 
@@ -73,37 +122,47 @@ def main():
                 max_future_q = np.max(q_table[new_discrete_state])
                 current_q = q_table[discrete_state + (action,  )]
 
-                new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+                new_q = (1 - learning_rate) * current_q + learning_rate * (reward + discount * max_future_q)
                 q_table[discrete_state + (action, )] = new_q
             elif new_state[0] >= env.goal_position:
-                print("Reached the goal on episode", episode)
+                if first_goal:
+                    print("Reached the goal on epoch", epoch)
+                    first_goal = False
                 q_table[discrete_state + (action, )] = 0
 
             discrete_state = new_discrete_state
 
-        if START_EPSILON_DECAYING <= episode <= END_EPSILON_DECAYING:
-            epsilon -= EPSILON_DECAY_VALUE
+        if epsilon_decaying_range[0] <= epoch <= epsilon_decaying_range[1]:
+            epsilon -= epsilon_decay_value
 
-        ep_rewards.append(episode_reward)
+        epoch_rewards.append(epoch_reward)
 
-        if not episode % SHOW_EVERY:
-            average_reward = sum(ep_rewards[-SHOW_EVERY:])/len(ep_rewards[-SHOW_EVERY:])
-            aggr_ep_rewards["ep"].append(episode)
-            aggr_ep_rewards["avg"].append(average_reward)
-            aggr_ep_rewards["min"].append(min(ep_rewards[-SHOW_EVERY:]))
-            aggr_ep_rewards["max"].append(max(ep_rewards[-SHOW_EVERY:]))
+        if not epoch % save_range:
+            average_reward = sum(epoch_rewards[-save_range:])/len(epoch_rewards[-save_range:])
+            aggr_ep_rewards["epoch"].append(epoch)
+            aggr_ep_rewards["avg_value"].append(average_reward)
+            aggr_ep_rewards["min_value"].append(min(epoch_rewards[-save_range:]))
+            aggr_ep_rewards["max_value"].append(max(epoch_rewards[-save_range:]))
 
-            print("Episode:", episode, "average:", average_reward, "min:", min(ep_rewards[-SHOW_EVERY:]), "max:", max(ep_rewards[-SHOW_EVERY:]))
+            #print("Epoch:", epoch, "average:", average_reward, "min:", min(epoch_rewards[-save_range:]), "max:", max(epoch_rewards[-save_range:]))
+    print("tot time:", time.time()-start)
 
     env.close()
-
-    plt.plot(aggr_ep_rewards["ep"], aggr_ep_rewards["avg"], label="avg")
-    plt.plot(aggr_ep_rewards["ep"], aggr_ep_rewards["min"], label="min")
-    plt.plot(aggr_ep_rewards["ep"], aggr_ep_rewards["max"], label="max")
-    plt.legend(loc=4)
-    plt.show()
+    plot_graph_result(aggr_ep_rewards["epoch"], aggr_ep_rewards["avg_value"], aggr_ep_rewards["max_value"], aggr_ep_rewards["min_value"])
 
 
- 
+
+def main():
+    for test in TEST_LIST:
+        launch_new_q_learning_test(
+            test["epochs"],
+            test["show_n_time"],
+            test["q_table_dimension"],
+            test["learning_rate"],
+            test["discount"],
+            test["epsilon"],
+            test["epsilon_decaying_range"]
+        )
+
 if __name__ == '__main__':
     main()
